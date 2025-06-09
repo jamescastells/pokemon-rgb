ac3b14192cb2c030cbf3245bf2e2879a2deaab8a
unknown <james_castells@hotmail.com>
2025-06-08 19:14:51 -0500

    solved more conflicts

--- scripts/VermilionCity.asm
+++ scripts/VermilionCity.asm
@@ -262,10 +262,6 @@ VermilionCitySailor1Text:
 	text_far _VermilionCitySailor1ShipSetSailText
 	text_end
 
-.ShipHasReturnedText:
-	text_far _VermilionCitySailor1ShipReturnedText
-	text_end
-
 VermilionCityGambler2Text:
 	text_far _VermilionCityGambler2Text
 	text_end
